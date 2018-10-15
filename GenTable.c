84d70bf31eda3aac62889d3c698c5bb5ce0c1cf9
dxing97 <danielxing97@gmail.com>
2018-10-15 09:03:30 -0400

    base GenTable completed
    need to work on verification of the generated table
    
    Signed-off-by: dxing97 <danielxing97@gmail.com>

--- GenTable.c
+++ GenTable.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "project3.h"
 
 
@@ -8,20 +9,51 @@
  *  1 - encrypt
  *  0 - decrypt
  *  -1 - do nothing
+ *
+ * table entry format:
+ * {16 byte head} {16 byte tail},\n
+ * total 35 bytes per line
  */
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+//    printf("Hello, World!\n");
 //    FILE *in  = fopen("input.txt", "r");
 //    FILE *out = fopen("output.txt", "w");
 //    do_crypt(in, out, 1);
 //    fclose(in);
 //    fclose(out);
-    unsigned char key[16] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-    unsigned char input[17] = "0123456789abcdef";
-    unsigned char output[17];
-    do_encrypt_string(&input, &output, &key, 1);
-    printf("encrypted: %s\n", output);
+    if(argc <= 1) {
+        printf("Usage: GenTable n\n");
+        return 0;
+    }
+
+//    verify_plaintext("\0\0\0\0\0\0\0\0\0\0\0\0\0\0def", 28);
+//    return 1;
+
+//    printf("(n/8)*8=%d, n=%d", (n/8)*8, n);
+
+    int n = atoi(argv[1]);
+//    printf("(n/8)*8=%d, n=%d", (n/8)*8, n);
+    int tablelen = 2* (1 << n/2);
+    struct table table;
+    printf("n: %d table size (kbytes): %d\n", n,
+            (int) sizeof(struct table_entry)*tablelen / 8);
+
+    if(!generate_table(&table, n)) {
+        printf("could not generate table\n");
+        return 1;
+    }
+
+    if(export_table(&table, "rainbow")) {
+        printf("could not export table to disk\n");
+        return 1;
+    }
+
+//    unsigned char key[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+//    unsigned char input[] = "0123456789abcdef";
+//    unsigned char output[17];
+//    hash(&input, &output, &key, 1);
+//    printf("encrypted: %s\n", output);
     return 0;
 }
 
--- project3.h
+++ project3.h
@@ -12,58 +12,253 @@
 #include <memory.h>
 
 /*
- * given an input string, encrypt/decrypt it and save it to the output string. string lengths are fixed length.
+ * reduction function
+ * todo: global/static EVP context to save exec time (might not be nessecary)
  */
-int do_encrypt_string(char *in, char *out, unsigned char *key, int do_encrypt) {
-    /* Allow enough space in output buffer for additional block */
-    unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
-    int inlen, outlen;
-    EVP_CIPHER_CTX *ctx;
-//    memcpy(inbuf, in, 16);
-    /*
-     * Bogus key and IV: we'd normally set these from
-     * another source.
-     */
-//    unsigned char key[] = "0123456789abcdef";
-//    unsigned char iv[17];
-//    memset(&iv, 0, 17);
 
-    /* Don't set key or IV right away; we want to check lengths */
-    ctx = EVP_CIPHER_CTX_new();
-    EVP_CipherInit_ex(ctx, EVP_aes_128_ecb(), NULL, NULL, NULL,
-                      do_encrypt);
-    OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
-//    OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
 
-    /* Now we can set key and IV */
-    EVP_CipherInit_ex(ctx, NULL, NULL, key, NULL , do_encrypt);
+struct table_entry {
+    char head[16];
+    char tail[16];
+};
 
-    if (!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, BUF_LENGTH)) {
-        /* Error */
-        EVP_CIPHER_CTX_free(ctx);
-        return 1;
+struct table {
+    struct table_entry *entries;
+    int tablelength;
+};
+
+int import_table();
+int export_table();
+int generate_table(struct table *table, int n);
+int generate_chain(struct table *table, int n, struct table_entry *chain);
+int search_table(struct table *table, int n, unsigned const char *target);
+int reduce(int n, unsigned char *out, unsigned const char *hash);
+int generate_random_plaintext(int n, unsigned char *plaintext);
+int hash(unsigned char *out, unsigned char *in, int do_encrypt);
+
+
+/*
+ * returns 0 if the plaintext is valid, 1 if not valid
+ * the first 128-n bits of plaintext must be 0
+ */
+int verify_plaintext(unsigned char *plaintext, int n) ;
+
+int import_table() {
+    return 0;
+}
+
+int export_table(struct table *table, char *filename) {
+    return 0;
+}
+
+int generate_table(struct table *table, int n) {
+    int hashcount = 0, ret;
+    struct table_entry *new_entry;
+    table->tablelength = 0;
+    table->entries = calloc((size_t) (1 << n/2), sizeof(struct table_entry)); //allocate all entries up front
+    if(table->entries == NULL) {
+        printf("error allocating entries while generating table\n");
+        return -1;
     }
 
-//    for (;;) {
-//        inlen = (int) fread(inbuf, 1, 1024, in);
-//        if (inlen <= 0)
-//            break;
-//        if (!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, inlen)) {
-//            /* Error */
-//            EVP_CIPHER_CTX_free(ctx);
-//            return 0;
+    while(table->tablelength < (1 << n/2)) {
+        hashcount = generate_chain(table, n,
+                &(table->entries[table->tablelength]));
+        if(hashcount == -1) {
+            printf("error generating new chain while generating table");
+            return -1;
+        }
+        table->tablelength++;
+        if(table->tablelength % 128 == 0)
+            printf("tablelength: %d\n", table->tablelength);
+    }
+
+    return hashcount;
+}
+
+int generate_chain(struct table *table, int n, struct table_entry *chain) {
+    int hashcount = -1, chainlength = 0;
+    int found;
+    int repeated = 0, location;
+    unsigned char head[16], current[16], current_hash[16], tail[16];
+
+    generate_random_plaintext(n, current);
+    strcpy(head, current);
+
+    while(chainlength < (1 <<n /2)) {
+//        printf("chainlength: %d\n", chainlength);
+        found = search_table(table, n, current);
+        switch(found) {
+            case 1:
+//                break;
+
+            case 2:
+//                generate_random_plaintext(n, current);
+//                strcpy(head, current);
+//                chainlength = 0;
+//                break;
+//                printf("found repeated element in table");
+                if(repeated == 0)
+                    location = chainlength;
+                repeated = 1;
+            case 0:
+                hashcount = hash(current_hash, current, 1);
+                reduce(n, current, current_hash);
+                chainlength++;
+                break;
+
+            default:
+                return -1;
+//                break;
+        }
+    }
+    if(repeated == 1)
+        printf("repeated at chainlength %d", location);
+    strncpy(chain->head, head, 16);
+    strncpy(chain->tail, tail, 16);
+
+    return hashcount;
+}
+
+/*
+ * given a plaintext, find if it's already in the table.
+ * return values:
+ * 0 - not found in table
+ * 1 - found in head
+ * 2 - found in tail
+ */
+int search_table(struct table *table, int n, unsigned const char *target) {
+    for(int i = 0; i < table->tablelength; i++) {
+        if(strncmp(target, table->entries[i].head, 128/8) == 0){
+//            printf("repeat at head pos %d", i);
+            return 1;
+        }
+        if(strncmp(target, table->entries[i].tail, 128/8) == 0) {
+//            printf("repeat at tail pos %d", i);
+            return 2;
+        }
+    }
+    return 0;
+}
+
+/*
+ * take the input hash and reduce it back into the password space
+ * todo: fix problem here
+ */
+int reduce(int n, unsigned char *out, unsigned const char *hash) {
+    //naive approach: mod by 2^n
+    memset(out, 0, 16);
+
+    for(int i = 16-n/8-1; i < 16; i++) {
+        out[i] = hash[i];
+    }
+
+    int diff = n - (n/8)*8;
+    if(diff) {
+        out[15-n/8] = hash[15-n/8] & (((1 << diff) - 1));
+//        int res = hash[15 - n/8] & ~((1 << (diff)) - 1);
+//        if(res != 0){
+//            printf("invalid plaintext found!\n");
+//            return 1;
 //        }
-//        fwrite(outbuf, 1, (size_t) outlen, out);
-//    }
+
+    }
+
+    if(verify_plaintext(out, n)){
+        printf("error with reduction function\n");
+        return 2;
+    }
+    return 0;
+}
+
+
+/*
+ * generate a BUF_LENGTH bit char array that satisfies the
+ * requirements for the password
+ */
+int generate_random_plaintext(int n, unsigned char *plaintext) {
+    for(int i = 0; i < 16 ; i++) {
+        if (i < 16 - n / 8 - 1) {
+            plaintext[i] = 0;
+        } else {
+            plaintext[i] = (unsigned char) rand();
+
+        }
+        if((n/8)*8 != n) {
+            plaintext[16-n/8 - 1] %= 0x10;
+        }
+    }
+    if(verify_plaintext(plaintext, n) != 0){
+        printf("error with random gen function\n");
+    }
+    return 0;
+}
+
+/*
+ * returns 0 if the plaintext is valid, 1 if not valid
+ * the first 128-n bits of plaintext must be 0
+ */
+int verify_plaintext(unsigned char *plaintext, int n) {
+    int tmp;
+    if(n == 128) {
+        return 0;
+    }
+    for(int i = 0; i < 16-n/8-1; i++) {
+        if(plaintext[i] != 0){
+            printf("invalid plaintext found!\n");
+            return 1;
+        }
+    }
+    int diff = n - (n/8)*8;
+    if((n/8)*8 != n) {
+        int res = plaintext[15 - n/8] & ~((1 << (diff)) - 1);
+        if(res != 0){
+            printf("invalid plaintext found!\n");
+            return 1;
+        }
+
+    }
+    return 0;
+}
+
+/*
+ * given an input string, encrypt/decrypt it and save it to the output
+ * string. string lengths are fixed length.
+ */
+int hash(unsigned char *out, unsigned char *in, int do_encrypt) {
+    static int hashcount = 0; //project requirement
+    unsigned char plaintext[16];
+    memset(&plaintext, 0, 16);
+    int outlen;
+    EVP_CIPHER_CTX *ctx;
+
+    ctx = EVP_CIPHER_CTX_new();
+
+    //use ecb for no IV/chaining
+    EVP_CipherInit_ex(ctx, EVP_aes_128_ecb(), NULL, NULL, NULL, do_encrypt);
+    OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
+
+    EVP_CipherInit_ex(ctx, NULL, NULL, in, NULL , do_encrypt);
+
+    /* perform the encryption */
+    if (!EVP_CipherUpdate(ctx, out, &outlen, plaintext, BUF_LENGTH)) {
+        /* Error */
+        EVP_CIPHER_CTX_free(ctx);
+        return 0;
+    }
+
     if (!EVP_CipherFinal_ex(ctx, out, &outlen)) {
         /* Error */
         EVP_CIPHER_CTX_free(ctx);
-        return 1;
+        return 0;
     }
-//    fwrite(outbuf, 1, (size_t) outlen, out);
+
+//    printf("result length plaintext bytes: %d\n", outlen);
 
     EVP_CIPHER_CTX_free(ctx);
-    return 0;
+
+    hashcount++;
+    return hashcount;
 }
 
 int do_crypt(FILE *in, FILE *out, int do_encrypt)
